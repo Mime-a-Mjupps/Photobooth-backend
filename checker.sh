619eabe7008dccc9e71ad3e4d7f363b8ea619420
Filip Nyquist <nyquistfilip@gmail.com>
2023-02-14 20:37:32 +0100

    Fixed -nostdin

--- checker.sh
+++ checker.sh
@@ -5,7 +5,7 @@ inotifywait -m "$dir" -e close_write --format '%w%f' |
     do
 	if [ "${fname##*.}" = "webm" ]; then
        		echo "Converting webm to mp4";
-		ffmpeg -i "$fname" -movflags faststart -profile:v high -level 4.2 -vsync vfr "${fname/webm/mp4}"
+		ffmpeg -nostdin -i "$fname" -movflags faststart -profile:v high -level 4.2 -vsync vfr "${fname/webm/mp4}"
 		echo "Done with convertion!";
 		rm "$fname";
    	fi
