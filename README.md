a84a0446df98f44ddcadfbcd81008a4ada71cd9a
Filip Nyquist <nyquistfilip@gmail.com>
2023-02-14 20:33:46 +0100

    Uploaded first codebase

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Photobooth-backend
 The backend for the photobooth service used at Studentpuben Villan.
+
+# Installation
+
+More to follow...
---
+++ checker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dir="content"
+inotifywait -m "$dir" -e close_write --format '%w%f' |
+    while IFS=' ' read -r fname
+    do
+	if [ "${fname##*.}" = "webm" ]; then
+       		echo "Converting webm to mp4";
+		ffmpeg -i "$fname" -movflags faststart -profile:v high -level 4.2 -vsync vfr "${fname/webm/mp4}"
+		echo "Done with convertion!";
+		rm "$fname";
+   	fi
+    done
